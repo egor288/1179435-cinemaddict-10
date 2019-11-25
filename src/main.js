54390f4762bc40ee65c87ec807f76f1ec6ac4225
egor <egor@MacBookPro.local>
2019-11-25 21:37:32 +0300

    start

--- src/main.js
+++ src/main.js
@@ -31,8 +31,15 @@ const renderFilters = () => {
 const renderFilms = () => {
   return `<section class="films-list">
       <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
-
       <div class="films-list__container">
+
+      </div>
+      <button class="films-list__show-more">Show more</button>
+    </section>`;
+};
+
+const renderFilmCard = () => {
+  return `
         <article class="film-card">
           <h3 class="film-card__title">The Dance of Life</h3>
           <p class="film-card__rating">8.3</p>
@@ -122,14 +129,55 @@ const renderFilms = () => {
             <button class="film-card__controls-item button film-card__controls-item--favorite film-card__controls-item--active">Mark as favorite</button>
           </form>
         </article>
-      </div>
-
-      <button class="films-list__show-more">Show more</button>
-    </section>`;
+      `;
 };
 
-const renderFilmCard = () => {
-  return `<article class="film-card">
+const renderExtraFilms = () => {
+  return `<section class="films-list--extra">
+      <h2 class="films-list__title">Top rated</h2>
+
+      <div class="films-list__container">
+        <article class="film-card">
+          <h3 class="film-card__title">The Man with the Golden Arm</h3>
+          <p class="film-card__rating">9.0</p>
+          <p class="film-card__info">
+            <span class="film-card__year">1955</span>
+            <span class="film-card__duration">1h 59m</span>
+            <span class="film-card__genre">Drama</span>
+          </p>
+          <img src="./images/posters/the-man-with-the-golden-arm.jpg" alt="" class="film-card__poster">
+          <p class="film-card__description">Frankie Machine (Frank Sinatra) is released from the federal Narcotic Farm in Lexington, Kentucky with a set of drums and a new outlook on…</p>
+          <a class="film-card__comments">18 comments</a>
+          <form class="film-card__controls">
+            <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
+            <button class="film-card__controls-item button film-card__controls-item--mark-as-watched  film-card__controls-item--active">Mark as watched</button>
+            <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
+          </form>
+        </article>
+
+         <article class="film-card">
+          <h3 class="film-card__title">The Great Flamarion</h3>
+          <p class="film-card__rating">8.9</p>
+          <p class="film-card__info">
+            <span class="film-card__year">1945</span>
+            <span class="film-card__duration">1h 18m</span>
+            <span class="film-card__genre">Mystery</span>
+          </p>
+          <img src="./images/posters/the-great-flamarion.jpg" alt="" class="film-card__poster">
+          <p class="film-card__description">The film opens following a murder at a cabaret in Mexico City in 1936, and then presents the events leading up to it in flashback. The Grea…</p>
+          <a class="film-card__comments">12 comments</a>
+          <form class="film-card__controls">
+            <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
+            <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
+            <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
+          </form>
+        </article>
+
+        <section class="films-list--extra">
+      <h2 class="films-list__title">Most commented</h2>
+
+      <div class="films-list__container">
+        <article class="film-card">
           <h3 class="film-card__title">Santa Claus Conquers the Martians</h3>
           <p class="film-card__rating">2.3</p>
           <p class="film-card__info">
@@ -145,11 +193,52 @@ const renderFilmCard = () => {
             <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
             <button class="film-card__controls-item button film-card__controls-item--favorite film-card__controls-item--active">Mark as favorite</button>
           </form>
-        </article>`;
+        </article>
+
+        <article class="film-card">
+          <h3 class="film-card__title">Made for Each Other</h3>
+          <p class="film-card__rating">5.8</p>
+          <p class="film-card__info">
+            <span class="film-card__year">1939</span>
+            <span class="film-card__duration">1h 32m</span>
+            <span class="film-card__genre">Comedy</span>
+          </p>
+          <img src="./images/posters/made-for-each-other.png" alt="" class="film-card__poster">
+          <p class="film-card__description">John Mason (James Stewart) is a young, somewhat timid attorney in New York City. He has been doing his job well, and he has a chance of bei…</p>
+          <a class="film-card__comments">56 comments</a>
+          <form class="film-card__controls">
+            <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
+            <button class="film-card__controls-item button film-card__controls-item--mark-as-watched">Mark as watched</button>
+            <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
+          </form>
+        </article>
+      </div>
+    </section>`;
 };
 
 const createLoadMoreButtonTemplate = () => {
   return `<button class="films-list__show-more">Show more</button>`;
+};
+
+const renderPoster = () => {
+  return `<img src="./images/posters/the-man-with-the-golden-arm.jpg" alt="" class="film-card__poster">
+          <p class="film-card__description">Frankie Machine (Frank Sinatra) is released from the federal Narcotic Farm in Lexington, Kentucky with a set of drums and a new outlook on…</p>
+          <a class="film-card__comments">18 comments</a>
+          <form class="film-card__controls">
+            <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist">Add to watchlist</button>
+            <button class="film-card__controls-item button film-card__controls-item--mark-as-watched film-card__controls-item--active">Mark as watched</button>
+            <button class="film-card__controls-item button film-card__controls-item--favorite">Mark as favorite</button>
+          </form>
+        </article>
+
+        <article class="film-card">
+          <h3 class="film-card__title">Santa Claus Conquers the Martians</h3>
+          <p class="film-card__rating">2.3</p>
+          <p class="film-card__info">
+            <span class="film-card__year">1964</span>
+            <span class="film-card__duration">1h 21m</span>
+            <span class="film-card__genre">Comedy</span>
+          </p>`;
 };
 
 const render = (container, template, place) => {
@@ -163,8 +252,13 @@ render(siteHeader, renderHeader(), `beforebegin`);
 render(siteMainElement, renderNavigation(), `beforeend`);
 render(siteMainElement, renderFilters(), `beforeend`);
 render(siteMainElement, renderFilms(), `beforeend`);
-for (let i = 0; i <= 5; i++) {
-  render(siteMainElement, renderFilmCard(), `beforeend`);
-}
+const filmsContaner = document.querySelector(`.films-list__container`);
+render(filmsContaner, renderFilmCard(), `beforeend`);
+render(siteMainElement, renderExtraFilms(), `beforeend`);
+
+// for (let i = 0; i <= 5; i++) {
+//   render(siteMainElement, renderPoster(), ``);
+// }
+
 const boardElement = siteMainElement.querySelector(`.board`);
 render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
